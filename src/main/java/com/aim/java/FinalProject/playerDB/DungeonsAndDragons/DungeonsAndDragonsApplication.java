e47712deda3bdc03d9aa7c3cc1bf5a24c248ff5d
CatKW <64162366+CatKW@users.noreply.github.com>
2021-02-14 14:05:56 -0600

    Update

---
+++ src/main/java/com/aim/java/FinalProject/playerDB/DungeonsAndDragons/DungeonsAndDragonsApplication.java
@@ -0,0 +1,13 @@
+package com.aim.java.FinalProject.playerDB.DungeonsAndDragons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonsAndDragonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonsAndDragonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aim/java/FinalProject/playerDB/DungeonsAndDragons/DungeonsAndDragonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aim.java.FinalProject.playerDB.DungeonsAndDragons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonsAndDragonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
